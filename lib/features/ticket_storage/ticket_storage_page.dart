110ad732675fcadb016a94ffceb7239b9009bdac
aratar <aratar006@gmail.com>
2023-04-09 15:23:22 +0700

    Создан аппбар, состояния экрана, кнопки

--- lib/features/ticket_storage/ticket_storage_page.dart
+++ lib/features/ticket_storage/ticket_storage_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:surf_flutter_study_jam_2023/widgets/ticket_item_widget.dart';
 
 /// Экран “Хранения билетов”.
 class TicketStoragePage extends StatelessWidget {
@@ -6,6 +7,47 @@ class TicketStoragePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const FlutterLogo();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Хранение билетов'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return TicketItemWidget(index: index);
+            },
+          ),
+        ),
+        floatingActionButton: const AddItemButton());
+  }
+}
+
+/// Виджет “Кнопки добавления элементов”.
+class AddItemButton extends StatelessWidget {
+  const AddItemButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton.extended(
+      onPressed: () {},
+      label: const Text('Добавить'),
+    );
+  }
+}
+
+/// Виджет “Пустого экрана”.
+class EmptyScreenWidget extends StatelessWidget {
+  const EmptyScreenWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Здесь пока ничего нет'),
+    );
   }
 }
