7cdf3e0ef7c0295010aee45859ec1bc384a3b1df
aratar <aratar006@gmail.com>
2023-04-09 17:56:42 +0700

    Долбавлена валидация

--- lib/widgets/add_ticket_modal_bottom_sheet_widget.dart
+++ lib/widgets/add_ticket_modal_bottom_sheet_widget.dart
@@ -1,38 +1,70 @@
 import 'package:flutter/material.dart';
 
-class AddTicketModalBottomSheetWidget extends StatelessWidget {
-  const AddTicketModalBottomSheetWidget({
+class BottomSheetWidget extends StatefulWidget {
+  const BottomSheetWidget({
     super.key,
   });
 
   @override
+  State<BottomSheetWidget> createState() => _BottomSheetWidgetState();
+}
+
+class _BottomSheetWidgetState extends State<BottomSheetWidget> {
+  final _formKey = GlobalKey<FormState>();
+  Uri? validUrl;
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding:
-          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-      child: SizedBox(
-        height: 240,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const SizedBox(
-                width: 300,
-                child: TextField(
-                    decoration: InputDecoration(
-                  labelText: 'fgg',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Padding(
+        padding:
+            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        child: SizedBox(
+          height: 240,
+          child: Center(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(
+                    width: 300,
+                    child: TextFormField(
+                      autofocus: true,
+                      decoration: const InputDecoration(
+                        labelText: 'Введите Url',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        ),
+                      ),
+
+                      ///Проверка правильности сслыки
+                      validator: (text) {
+                        if (text == null || text.isEmpty) {
+                          return 'Пустое поле';
+                        }
+                        validUrl = Uri.tryParse(text);
+                        if (validUrl == null || validUrl!.scheme.isEmpty) {
+                          return 'Введите корректный Url';
+                        }
+                        return null;
+                      },
+                    ),
                   ),
-                )),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        print(' Форма: $validUrl');
+                      }
+                    },
+                    child: const Text('Добавить'),
+                  )
+                ],
               ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () => Navigator.pop(context),
-                child: const Text('Добавить'),
-              )
-            ],
+            ),
           ),
         ),
       ),
